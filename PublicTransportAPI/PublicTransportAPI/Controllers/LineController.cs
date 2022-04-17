7cf642b78d7e76ca66babfad51020a7676b0c049
Noweli <patryklewonn@gmail.com>
2022-04-17 23:28:28 +0200

    Added Line Controller class, modified .gitignore

---
+++ PublicTransportAPI/PublicTransportAPI/Controllers/LineController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PublicTransportAPI.Data;
+
+namespace PublicTransportAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class LineController
+{
+    private readonly ApplicationDbContext _dbContext;
+    
+    public LineController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+}
