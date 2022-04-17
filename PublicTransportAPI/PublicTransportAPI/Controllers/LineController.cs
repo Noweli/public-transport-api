a6a83debe3c1ee51e3fb5db7aa0ea8bded6f0cc6
Noweli <patryklewonn@gmail.com>
2022-04-17 23:47:25 +0200

    Added LineController.cs implementation - add

--- PublicTransportAPI/PublicTransportAPI/Controllers/LineController.cs
+++ PublicTransportAPI/PublicTransportAPI/Controllers/LineController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PublicTransportAPI.Data;
+using PublicTransportAPI.Data.DTOs;
+using PublicTransportAPI.Data.Models;
 
 namespace PublicTransportAPI.Controllers;
 
@@ -13,5 +15,49 @@ public class LineController
     {
         _dbContext = dbContext;
     }
+
+    [HttpPut]
+    public async Task<ActionResult<Line>> Add([FromBody] LineDTO lineDTO)
+    {
+        if (string.IsNullOrWhiteSpace(lineDTO.LineIdentifier))
+        {
+            return new BadRequestObjectResult("Line identifier not provided.");
+        }
+
+        Line addedLineResult;
+
+        try
+        {
+            var stopPointIds = Array.Empty<StopPoint>();
+
+            if (lineDTO.StopPointIds is not null && lineDTO.StopPointIds.Any())
+            {
+                var result = _dbContext.StopPoints!.Where(point => lineDTO.StopPointIds.Contains(point.Id));
+                
+                if (!result.Any())
+                {
+                    return new BadRequestObjectResult("Could not find line stops with selected ids.");
+                }
+
+                stopPointIds = result.ToArray();
+            }
+
+            var resultAdded = await _dbContext.Lines!.AddAsync(new Line
+            {
+                LineIdentifier = lineDTO.LineIdentifier,
+                StopPoints = stopPointIds
+            });
+            _ = await _dbContext.SaveChangesAsync();
+
+            addedLineResult = resultAdded.Entity;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return new OkObjectResult(addedLineResult);
+    } 
     
 }
---
+++ PublicTransportAPI/PublicTransportAPI/Data/DTOs/LineDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace PublicTransportAPI.Data.DTOs;
+
+public class LineDTO
+{
+    public string? LineIdentifier { get; set; }
+    public virtual ICollection<int>? StopPointIds { get; set; }
+}
