da5112f48937334bc4107bf013fa71125865dbee
Noweli <patryklewonn@gmail.com>
2022-04-18 14:42:00 +0200

    Added AutoMapper

---
+++ PublicTransportAPI/PublicTransportAPI/Mapper/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using PublicTransportAPI.Data.DTOs;
+using PublicTransportAPI.Data.Models;
+
+namespace PublicTransportAPI.Mapper;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<StopPointLineEvent, StopPointLineEventDTO>().ReverseMap();
+    }
+}
--- PublicTransportAPI/PublicTransportAPI/Program.cs
+++ PublicTransportAPI/PublicTransportAPI/Program.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using PublicTransportAPI.Data;
+using PublicTransportAPI.Mapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("datasource=database.sql"));
+builder.Services.AddAutoMapper(Assembly.GetAssembly(typeof(MappingProfile)));
 
 var app = builder.Build();
 
