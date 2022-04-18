0dd72b54bd18420513acbfded886491013047253
Noweli <patryklewonn@gmail.com>
2022-04-18 14:52:07 +0200

    Modified StopPointLineEventDTO.cs

---
+++ PublicTransportAPI/PublicTransportAPI/Data/DTOs/StopPointLineEventDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace PublicTransportAPI.Data.DTOs;
+
+public class StopPointLineEventDTO
+{
+    public string? Arrival { get; set; }
+    public string? Departure { get; set; }
+    public virtual int? LineId { get; set; }
+    public virtual int? StopPointId { get; set; }
+}
