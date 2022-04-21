4ba9a8ee7ddb275395533996c9cdf4755eb67e77
Noweli <patryklewonn@gmail.com>
2022-04-21 20:29:54 +0200

    Added StopPointController tests along with corrections

---
+++ PublicTransportAPI/PublicTransportAPI.Tests/Controllers/StopPointControllerTests.cs
@@ -0,0 +1,203 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using PublicTransportAPI.Controllers;
+using PublicTransportAPI.Data;
+using PublicTransportAPI.Data.Models;
+
+namespace PublicTransportAPI.Tests.Controllers;
+
+[TestFixture]
+public class StopPointControllerTests
+{
+    private ApplicationDbContext? _dbContext;
+    private StopPointController? _stopPointController;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var dbOptionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase("database");
+        _dbContext = new ApplicationDbContext(dbOptionsBuilder.Options);
+        _stopPointController = new StopPointController(_dbContext);
+
+        PrepareDatabaseRecord();
+    }
+
+    private void PrepareDatabaseRecord()
+    {
+        if (_dbContext!.StopPoints!.Any())
+        {
+            return;
+        }
+
+        _dbContext.StopPoints!.Add(new StopPoint {Name = "test"});
+        _dbContext.StopPoints!.Add(new StopPoint {Name = "test2"});
+        _dbContext.SaveChanges();
+    }
+
+    [Test]
+    public async Task AddMethod_NameIsEmpty_AssertionFail()
+    {
+        //Arrange
+        var name = string.Empty;
+        
+        //Act
+        var result = await _stopPointController!.Add(name);
+        
+        //Assert
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+    
+    [Test]
+    public async Task AddMethod_NameIsNull_AssertionFail()
+    {
+        //Arrange
+        string? name = null;
+        
+        //Act
+        var result = await _stopPointController!.Add(name!);
+        
+        //Assert
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+
+    [Test]
+    public async Task AddMethod_DbContextIsNull_ReturnBadRequest()
+    {
+        //Arrange
+        const string name = "test";
+        _dbContext = null;
+        _stopPointController = new StopPointController(_dbContext!);
+        
+        //Act
+        var result = await _stopPointController!.Add(name);
+        
+        //Assert
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+    
+    [Test]
+    public async Task AddMethod_NameProvidedCorrectly_Success()
+    {
+        //Arrange
+        const string name = "test";
+
+        //Act
+        var result = await _stopPointController!.Add(name);
+        
+        //Assert
+        result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Test]
+    public async Task GetMethod_IdIsNegative_AssertionFail()
+    {
+        //Arrange
+        const int id = -1;
+        
+        //Act
+        var result = await _stopPointController!.GetPoint(id);
+        
+        //Assert
+        result.Result.Should().BeOfType<BadRequestObjectResult>();
+    }
+    
+    [Test]
+    public async Task GetMethod_IdProvidedButIncorrect_AssertionFail()
+    {
+        //Arrange
+        const int id = int.MaxValue;
+        
+        //Act
+        var result = await _stopPointController!.GetPoint(id);
+        
+        //Assert
+        result.Result.Should().BeOfType<BadRequestObjectResult>();
+    }
+    
+    [Test]
+    public async Task GetMethod_IdProvidedAndCorrect_Success()
+    {
+        //Arrange
+        const int id = 1;
+        
+        //Act
+        var result = await _stopPointController!.GetPoint(id);
+        
+        //Assert
+        result.Value.Should().BeOfType<StopPoint>();
+    }
+    
+    [Test]
+    public async Task GetMethod_DbContextIsNull_ReturnBadRequest()
+    {
+        //Arrange
+        const int id = 1;
+        _dbContext = null;
+        _stopPointController = new StopPointController(_dbContext!);
+        
+        //Act
+        var result = await _stopPointController!.GetPoint(id);
+        
+        //Assert
+        result.Result.Should().BeOfType<BadRequestObjectResult>();
+    }
+    
+    [Test]
+    public async Task DeleteMethod_IdIsNegative_AssertionFail()
+    {
+        //Arrange
+        const int id = -1;
+
+        //Act
+        var result = await _stopPointController!.DeletePoint(id);
+        
+        //Assert
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+    
+    [Test]
+    public async Task DeleteMethod_IdProvidedButIncorrect_AssertionFail()
+    {
+        //Arrange
+        const int id = int.MaxValue;
+
+        //Act
+        var result = await _stopPointController!.DeletePoint(id);
+        
+        //Assert
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+    
+    [Test]
+    public async Task DeleteMethod_IdProvidedAndCorrect_Success()
+    {
+        //Arrange
+        const int id = 2;
+
+        //Act
+        var result = await _stopPointController!.DeletePoint(id);
+        
+        //Assert
+        result.Should().BeOfType<OkResult>();
+    }
+    
+    [Test]
+    public async Task DeleteMethod_DbContextIsNull_ReturnBadRequest()
+    {
+        //Arrange
+        const int id = 1;
+        _dbContext = null;
+        _stopPointController = new StopPointController(_dbContext!);
+
+        //Act
+        var result = await _stopPointController!.DeletePoint(id);
+        
+        //Assert
+        result.Should().BeOfType<BadRequestObjectResult>();
+    }
+}
--- PublicTransportAPI/PublicTransportAPI/Controllers/StopPointController.cs
+++ PublicTransportAPI/PublicTransportAPI/Controllers/StopPointController.cs
@@ -23,21 +23,19 @@ public class StopPointController
             return new BadRequestObjectResult("Failed to add new stop point. Name has to be provided.");
         }
 
-        StopPoint resultObject = new();
-
         try
         {
             var result = await _dbContext.StopPoints!.AddAsync(new StopPoint {Name = name});
             _ = await _dbContext.SaveChangesAsync();
-            
-            resultObject = result.Entity;
+
+            return new OkObjectResult(result.Entity);
         }
         catch (Exception e)
         {
             await Console.Out.WriteLineAsync($"Failed to add new stop point. Exception message: {e.Message}");
         }
 
-        return new OkObjectResult(resultObject);
+        return new BadRequestObjectResult("Failed to add new stop point. Check logs.");
     }
 
     [HttpGet]
@@ -48,25 +46,23 @@ public class StopPointController
             return new BadRequestObjectResult("Id index cannot be lower than zero.");
         }
 
-        StopPoint resultObject = new();
-
         try
         {
             var result = await _dbContext.StopPoints!.FindAsync(id);
 
             if (result is null)
             {
-                return new NotFoundObjectResult("Could not find point with specified id.");
+                return new BadRequestObjectResult("Could not find point with specified id.");
             }
 
-            resultObject = result;
+            return result;
         }
         catch (Exception e)
         {
             await Console.Out.WriteLineAsync($"Exception occured while gathering information about point with id {id}. Error message: {e.Message}");
         }
 
-        return resultObject;
+        return new BadRequestObjectResult("Could not find stop point. Check logs.");
     }
 
     [HttpDelete]
@@ -83,17 +79,19 @@ public class StopPointController
 
             if (result is null)
             {
-                return new NotFoundObjectResult($"Could not find stop point with id {id}.");
+                return new BadRequestObjectResult($"Could not find stop point with id {id}.");
             }
 
             _ = _dbContext.StopPoints!.Remove(result);
             _ = await _dbContext.SaveChangesAsync();
+
+            return new OkResult();
         }
         catch (Exception e)
         {
             await Console.Out.WriteLineAsync($"Could not delete stop point with id {id}. Error message: {e.Message}");
         }
 
-        return new OkResult();
+        return new BadRequestObjectResult("Failed to delete stop point. Check logs.");
     }
 }
